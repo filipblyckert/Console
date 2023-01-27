78da87740ad3cd3d7bf6788d4bb785dfe778cc27
filipblyckert <filipblyckert@icloud.com>
2023-01-27 10:17:23 +0100

    remove

---
+++ adressbok/AdressBook.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adressbok
+{
+    internal class AdressBook
+    {
+        private List<Contact> _contacts { get; set; } = new List<Contact>();
+        private void DisplayContactDetails(Contact contact)
+        {
+            Console.WriteLine($"Contact:{contact.Name}, {contact.Number}, {contact.Email}, {contact.Adress}");
+        }
+
+        public void AddContact(Contact contact)
+        {
+            _contacts.Add(contact);
+        }
+
+        private void DisplayContactsDetails(List<Contact> contacts) 
+        {
+            foreach (var contact in contacts)
+            {
+                DisplayContactDetails(contact);
+            }
+        }
+      
+
+        public void DisplayAllContact()
+        {
+            DisplayContactsDetails(_contacts);
+        }
+
+        public void DisplayMatchingContacts(string searchPhrase)
+        {
+            var matchingContacts = _contacts.Where(c => c.Name.Contains(searchPhrase)).ToList();
+            if (matchingContacts != null)
+            {
+                Console.WriteLine("Contact not found");
+            }
+            else
+            {
+               DisplayContactsDetails(matchingContacts);
+            }
+            
+        }
+    }   
+}
---
+++ adressbok/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adressbok
+{
+    internal class Contact
+    {
+       public Contact(string name, string number, string email, string adress) 
+        {
+            Name = name;
+            Number = number;
+            Email = email;
+            Adress = adress;
+        }
+
+        public string Number { get; internal set; }
+        public string Name { get; internal set; }
+        public string Adress { get; internal set; }
+        public string Email { get; internal set; }
+    }
+}
